c241c1f98a54a8bea9363d0df1f591018129ea8b
thephilosopher13 <60373194+thephilosopher13@users.noreply.github.com>
2023-06-13 17:01:57 +0800

    Create skeleton initializier buttons and divs

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/*
+
+1. Create gameBoard module (probably best done through a DOM constructor after loading)
+1.1 create array inside a Gameboard object
+1.1.1 store gameboard inside array
+2. create display controller
+3. create object to control flow of game
+3.1. have a thing that determines whose turn is it.
+3.2. create logic that ends game
+4. Create a factory that creates player 1 and player 2
+4.1. (do this later) allow it to create AI Also.
+
+
+
+*/
